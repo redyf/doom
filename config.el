e12d550a6d368b1a136fff307bc04c340285ff4d
redyf <mateusalvespereira7@gmail.com>
2024-12-07 20:52:54 -0300

    feat: add new config

---
+++ config.el
@@ -0,0 +1,117 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+(setq confirm-kill-emacs nil)
+(setq inhibit-startup-message t)
+(setq debug-on-error t)
+(scroll-bar-mode -1) ; Disable visible scrollbar
+(tool-bar-mode -1) ; Disable the toolbar
+(tooltip-mode -1) ; Disable tooltips
+(set-fringe-mode 10) ; Give some breathing room
+(menu-bar-mode -1) ; Disable the menu bar
+
+;; If your gpu has vsync on or you’re experiencing flickering
+(add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
+
+;; Set up the visible bell
+;; (setq visible-bell t)
+(setq ring-bell-function 'ignore)
+
+(column-number-mode)
+(global-display-line-numbers-mode 1)
+(setq display-line-numbers-type 'relative)  ;; Use relative line numbers
+(setq display-line-numbers-width 4)  ;; Set the width of line numbers to 4 digits
+
+;; Set the idle update delay to increase FPS
+(setq idle-update-delay 0.01) ;; 100 FPS (0.01 seconds delay between updates)
+
+;; Enable pixel-perfect precision scrolling
+(pixel-scroll-precision-mode 1)
+(setq pixel-scroll-precision-interpolate-page t    ;; Smooth page scrolling
+      pixel-scroll-precision-large-scroll-height 40 ;; Set height for large scrolls
+      pixel-scroll-precision-mode t)                ;; Enable for the entire mode
+
+;; Prevent Emacs from pausing during redisplay to improve animation smoothness
+(setq redisplay-dont-pause t)
+
+(global-set-key (kbd "C-S-v") 'yank)
+
+(set-face-attribute 'default nil :font "Berkeley Mono" :height 120)
+
+(map! :n [tab]   #'next-buffer)
+(map! :n [backtab] #'previous-buffer)
+
+;; TODO: Switch tabs with tab
+;; TODO: Baixar key-chord
+
+(require 'elcord)
+(elcord-mode)
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
